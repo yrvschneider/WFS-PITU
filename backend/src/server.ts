9f0517a53f50e03985c8570476edf7c22a170c9a
yrvschneider <yrvschneider@gmail.com>
2020-11-25 14:25:08 -0300

    commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,5 +4,5 @@ import database from './database';
 database.sync();
 console.log('Databese running at 3306');
 
-app.listen(3000);
-console.log('Server running at 3000');
+app.listen(3001);
+console.log('Server running at 3001');
